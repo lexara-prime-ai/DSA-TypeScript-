234172438cd3552f669184b8089d6a516a3c51a3
Irfan Ghat <58285388+projectfinalaudio@users.noreply.github.com>
2023-05-16 18:18:08 +0300

    Update numberReversal.test.js

--- tests/numberReversal.test.js
+++ tests/numberReversal.test.js
@@ -18,3 +18,30 @@ describe('reverseInput', () => {
         expect(reverseInput(0)).to.equal(0);
     });
 });
+
+////////////////////////////////////
+/////////// DESCRIPTION ///////////
+////////////////////////////////////
+
+// In this example, we use the describe function to group the test cases for 
+// the reverseInput function. Each test case is defined using the it function, 
+// which describes the expected behavior.
+
+// The first test case checks if the reverseInput function correctly reverses a 
+// positive number (12345) and expects the result to be 54321.
+
+// The second test case checks if the function handles negative numbers correctly by 
+// reversing -9876 and expecting the result to be -6789.
+
+// The third test case verifies if the function correctly handles the input of 0 
+// and expects the reversed number to be 0 as well.
+
+// To run these tests using Jest, you need to have Jest installed and configured 
+// in your project. Then you can execute the tests using the jest command in your 
+// terminal or by running your test script.
+
+
+
+
+
+
