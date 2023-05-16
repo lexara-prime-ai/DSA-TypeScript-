f163864a3ad999b12cf80d746679575181d391b1
irfanghat <irfanghat@gmail.com>
2023-05-16 17:50:20 +0300

    Beginner: Data Structure and Algorithms

--- tests/numberReversal.test.js
+++ tests/numberReversal.test.js
@@ -3,16 +3,17 @@
 ///////////////////////////
 import { expect } from 'chai';
 import { reverseInput } from './reverseInput'; 
-
+// PASS IN EXPORTED FUNCTION AS A PARAMETER
 describe('reverseInput', () => {
+    // CHECK IF RESULT IS EQUAL TO THE REVERSED INPUT + POSITIVE NUMBERS
     it('should reverse a positive number', () => {
         expect(reverseInput(12345)).to.equal(54321);
     });
-
+    // CHECK IF RESULT IS EQUAL TO THE REVERSED INPUT - NEGATIVE NUMBERS
     it('should reverse a negative number', () => {
         expect(reverseInput(-9876)).to.equal(-6789);
     });
-
+    // CASE WHERE IPUT IS EQUAL TO 0
     it('should reverse zero', () => {
         expect(reverseInput(0)).to.equal(0);
     });
